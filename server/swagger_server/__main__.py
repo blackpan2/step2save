12fbaaa8694e1b66bb5ab0457a4e3c7135451d7a
unknown <shedin422@gmail.com>
2018-01-28 02:41:41 +0000

    basic ass database implementation

--- server/swagger_server/__main__.py
+++ server/swagger_server/__main__.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
+import os
+import sys
+lib_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'database/'))
+sys.path.append(lib_path)
 
 import connexion
 from .encoder import JSONEncoder
+import database
 
 
 if __name__ == '__main__':
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'API to support the mobile applciation step2save'})
+    database.addProduct('one', 'two')
+    database.printProducts()
     app.run(port=8080)
